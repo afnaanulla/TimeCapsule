153b8ceef321b14662225262bb327c24cd9d6538
MohammadAfnaan <afnaan31250@gmail.com>
2025-01-24 17:31:02 +0530

    Initilize project setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 2004;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
