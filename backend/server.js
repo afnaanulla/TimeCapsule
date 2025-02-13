8a35fb4c9caf3096691dc3512d63a471d558470a
MohammadAfnaan <afnaan31250@gmail.com>
2025-02-14 00:09:31 +0530

    fff

--- backend/server.js
+++ backend/server.js
@@ -61,10 +61,7 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 2004;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
 
