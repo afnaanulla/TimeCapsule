744aa9fca16600743d087f05f2b2548d20823513
afnaanulla <muhmmadafnaan2003@gmail.com>
2025-02-13 22:36:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 const corsOptions = {
 
-  origin: ['http://localhost:4200',],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
